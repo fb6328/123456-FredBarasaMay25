c57f2e758f4185d7539735c4f797fcb06e655cc3
Bim <fb6328@gmail.com>
2025-06-09 20:18:31 +0300

    Updates v1

---
+++ HelloWorld.cpp
@@ -0,0 +1,9 @@
+// This is my first C++ program
+#include <iostream> // Include the input-output stream library
+using namespace std;
+int main()
+{
+    cout << "Hello, World!" << endl; // Output a greeting message
+    cout << "" << endl;
+    return 0; // Indicate that the program ended successfully
+}
